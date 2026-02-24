8e8807f9afc7d7cc4ee6570e51c95b60b9d1b69d
Enes Kızılca <eneskizilca@users.noreply.github.com>
2026-02-24 20:48:51 +0300

    chore : initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB -1</h2>
+      <p>Ad Soyad: Enes Kizilca</p>
+      <p>Öğrenci No: 235541116</p>
+    </div>
+  )
+}
+
+export default App
